15edaeb1d03f62ffa83f2efe9ffd80cafb84fb8e
jcmunav63 <jcmunav63@gmail.com>
2024-08-27 17:39:40 -0600

    Editar codigo en página html, edición de estilos, y creación de funciones y métodos en JavaScript.

--- index.js
+++ index.js
@@ -1,1 +1,114 @@
-// Código JavaScript
+const botonEncriptar = document.querySelector(".btn-encriptar");
+const textoEncriptar = document.querySelector(".encriptar");
+const aviso = document.querySelector(".textalert");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector(".tarjeta-visualizador");
+const botonCopiar = document.querySelector(".btn-copiar");
+const botonDesencriptar = document.querySelector(".btn-desencriptar");
+
+botonEncriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = textoEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+    
+    if(texto == ""){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El campo de texto está vacio";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== txt){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No usar caracteres especiales";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "Solo letras minúsculas y sin acentos";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/e/mg, "enter");
+        texto = texto.replace(/i/mg, "imes");
+        texto = texto.replace(/a/mg, "ai");
+        texto = texto.replace(/o/mg, "ober");
+        texto = texto.replace(/u/mg, "ufat");
+
+        respuesta.innerHTML = texto;
+        botonCopiar.style.visibility = "inherit";
+        contenido.remove(); 
+    }
+});
+
+botonDesencriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = textoEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+    
+    if(texto == ""){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El campo de texto no debe estar vacio";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== txt){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No debe tener acentos y caracteres especiales";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El texto debe ser todo en minúscula";
+        
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/enter/mg, "e");
+        texto = texto.replace(/imes/mg, "i");
+        texto = texto.replace(/ai/mg, "a");
+        texto = texto.replace(/ober/mg, "o");
+        texto = texto.replace(/ufat/mg, "u");
+
+        respuesta.innerHTML = texto;
+        botonCopiar.style.visibility = "inherit";
+        contenido.remove(); 
+    }
+});
+
+botonCopiar.addEventListener("click", copiar = () => {
+    var contenido = document.querySelector(".evaluar").textContent;
+    navigator.clipboard.writeText(contenido);
+})
